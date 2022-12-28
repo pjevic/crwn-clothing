ed4556130e67ebb45ce7d2f6d70375db33919f9a
pjevic <milanpjevic@icloud.com>
2022-12-28 17:15:02 +0100

    Styled Component - Form Input

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,11 +1,11 @@
-import "./form-input.style.scss";
+import { FormInputLabel, Input, Group } from "./form-input.style.jsx";
 
 const FormInput = ({ label, inputOptions }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...inputOptions} />
-      {label && <label className={`${inputOptions.value.length ? "shrink" : ""} form-input-label`}>{label}</label>}
-    </div>
+    <Group>
+      <Input {...inputOptions} />
+      {label && <FormInputLabel shrink={inputOptions.value.length}>{label}</FormInputLabel>}
+    </Group>
   );
 };
 
