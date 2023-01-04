6202dc7c75e09f5c59e36dd16d75db1275e5d98d
pjevic <milanpjevic@icloud.com>
2023-01-04 16:18:52 +0100

    Implementing Redux-Thunk

--- src/store/store.js
+++ src/store/store.js
@@ -17,6 +17,7 @@ import { legacy_createStore as createStore } from "redux";
 import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
@@ -28,7 +29,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== "development" && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(Boolean);
 
 const composeEnchancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
