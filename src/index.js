bfd64a416e6cb18b049b8e03eb5fedc008fbd405
pjevic <milanpjevic@icloud.com>
2023-01-05 22:00:55 +0100

    stripe

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import reportWebVitals from "./reportWebVitals";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
+
+import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,7 +18,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
